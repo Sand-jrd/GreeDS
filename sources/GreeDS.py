1331c4f413b0e9523c1274e62230d6702f47969b
Sandrine Juillard <47563242+Sand-jrd@users.noreply.github.com>
2024-02-13 16:19:40 +0100

    Update GreeDS.py
    Add the "incr" option
    Remove unused import
    Fix some notations for prettiness
    Fix wrap to switch from fft rotation to interpolation rotation

--- sources/GreeDS.py
+++ sources/GreeDS.py
@@ -19,16 +19,19 @@ Require the dependancy torch and kornia
 import torch
 import numpy as np
 from rotation import tensor_rotate_fft
-from torchvision.transforms.functional import rotate
-from torchvision.transforms.functional import InterpolationMode
-from vip_hci.fits import write_fits 
+from torchvision.transforms.functional import (rotate, InterpolationMode)
 
-def cube_rotate(cube, angles, mode="fft"):
+def cube_rotate(cube, angles, fft=False):
     new_cube = torch.zeros(cube.shape)
-    for ii in range(len(angles)):
-        new_cube[ii] = rotate(torch.unsqueeze(cube[ii], 0),-float(angles[ii]),
+    if fft:
+        for ii in range(len(angles)):
+            new_cube[ii] = rotate(torch.unsqueeze(cube[ii], 0), -float(angles[ii]),
                                   InterpolationMode.BILINEAR)[0]
-    return new_cube
+        return new_cube
+    else:
+        for ii in range(len(angles)):
+            new_cube[ii] = tensor_rotate_fft(cube[ii], float(angles[ii]))
+        return new_cube
 
 
 def circle(shape: tuple, r: float, offset=(0.5, 0.5)):
@@ -69,11 +72,13 @@ def circle(shape: tuple, r: float, offset=(0.5, 0.5)):
     return 1 - M
 
 
-def GreeDS(cube, angles, r=1, l=10, r_start=1, pup=6, RDI=None, full_output=0, returnL=False):
+def GreeDS(cube, angles, r=1, l=10, r_start=1, pup=6, refs=None, x_start=None, full_output=0, returnL=False,
+           returntype="numpy"):
     """
 
     Parameters
     ----------
+    x_start
     cube : numpy array
         3D cube of data. shape : (nb_frame, length, width)
         
@@ -83,9 +88,10 @@ def GreeDS(cube, angles, r=1, l=10, r_start=1, pup=6, RDI=None, full_output=0, r
     r : int
         Number of rank to iterate over. The default is 1.
         
-    l : int
+    l : int or str {'incr'}
         Number of iteration per rank. The default is 10.
-        
+        If set to 'incr', the number of iteration per rank will increase with rank.
+
     r_start : int
         First rank estimate, r_start < r
         GreeDS will iterate from rank r-start to r 
@@ -93,9 +99,12 @@ def GreeDS(cube, angles, r=1, l=10, r_start=1, pup=6, RDI=None, full_output=0, r
     pup : int
         Raduis of the pupil mask
         
-    RDI : numpy array
+    refs : numpy array
         3D cube of reference frames. shape = (nb_frame, length, width)
-        
+
+    returntype : {"numpy", "tensor"}
+        Type of the function output
+
     full_output : int (0 to  3)
         Choose to return :  
         *  0/False -> only last estimation 
@@ -122,44 +131,44 @@ def GreeDS(cube, angles, r=1, l=10, r_start=1, pup=6, RDI=None, full_output=0, r
     nb_frame = len(angles)
     nb_frame_es = len(angles)
 
-    # RDI
-    if RDI is not None : 
-        assert(RDI.shape[-2:] == shape)
-        RDI = torch.from_numpy(RDI)
-        print("Cube filled with "+str(int(100*RDI.shape[0]/nb_frame))+" percent of RDI frames")
-        nb_frame_es = len(angles) + RDI.shape[0]
+    # References
+    if refs is not None:
+        assert (refs.shape[-2:] == shape)
+        refs = torch.from_numpy(refs)
+        print("Cube filled with " + str(int(100 * refs.shape[0] / nb_frame)) + " percent of reference frames")
+        nb_frame_es = len(angles) + refs.shape[0]
 
     # Convert to use torch
     cube = torch.from_numpy(cube)
 
     angles = torch.from_numpy(angles)
-    pup = circle(shape, pup)
-    
-    # Init variables
-    if full_output == 1 : iter_frames = torch.zeros((l * (r-r_start+1),) + shape)
-    elif full_output == 2 : iter_frames = torch.zeros((r,) + shape)
-    elif full_output == 3 : iter_frames = torch.zeros((l,) + shape)
+    pup = 1 if pup == 0 else circle(shape, pup)
+
+    iter_frames = []
+    iter_L = []
 
     x_k = torch.zeros(shape)
+    if x_start is not None: x_k = torch.from_numpy(x_start)
 
+    incr = True if l == "incr" else False
 
     # One iteration of greeDS
     def GreeDS_iter(x, q):
 
-        
         R = cube - cube_rotate(x.expand(nb_frame, len_img, len_img), -angles)
 
-        if RDI is not None :
-            R = torch.cat((R,RDI))
+        if refs is not None:
+            R = torch.cat((R, refs))
 
-        U, Sigma, V = torch.pca_lowrank(R.view(nb_frame_es, len_img * len_img), q=q, niter=5, center=False)
+        U, Sigma, V = torch.pca_lowrank(R.view(nb_frame_es, len_img * len_img), q=q, niter=1, center=False)
         L = (U @ torch.diag(Sigma) @ V.T).reshape(nb_frame_es, len_img, len_img)
 
-        if RDI is not None : L = L[:nb_frame]
+        if refs is not None: L = L[:nb_frame]
+        L *= L > 0
 
         S_der = cube_rotate(cube - L, angles)
 
-        frame = torch.mean(S_der, axis=0)*pup
+        frame = torch.mean(S_der, axis=0) * pup
         frame *= frame > 0
 
         return frame, L
@@ -167,25 +176,32 @@ def GreeDS(cube, angles, r=1, l=10, r_start=1, pup=6, RDI=None, full_output=0, r
     ## Main loop over N_comp and nb_rank.
     for ncomp in range(r_start, r + 1):
 
-        for ii in range(1, l + 1):
+        if incr: l = ncomp - r_start + 1
+
+        for _ in range(1, l + 1):
 
             x_k1, xl = GreeDS_iter(x_k, ncomp)
             x_k = x_k1.clone()
 
-            if full_output == 1 : iter_frames[(ncomp - r_start) * l + (ii - 1), :, :] = x_k1
-            if full_output == 3 : iter_frames[ii - 1, :, :] = x_k1
+            if full_output == 1:
+                iter_frames.append(x_k1.numpy())
+                if returnL: iter_L.append(xl.numpy())
+            if full_output == 3 and ncomp == r + 1: iter_frames.append(x_k1.numpy())
 
-        if full_output == 2 : iter_frames[ncomp - r_start, :, :] = x_k1
+        if full_output == 2: iter_frames.append(x_k1.numpy())
 
-    
-    if returnL :
-        
+    iter_frames = np.array(iter_frames)
+    iter_L = np.array(iter_L)
+    if returntype == "numpy":
+        x_k = x_k.numpy()
+        xl = xl.numpy()
+    if returnL:
         if full_output:
-            return iter_frames.numpy(), xl.numpy()
+            return iter_frames, iter_L
         else:
-            return x_k.numpy(), xl.numpy()
-        
+            return x_k, xl
+
     if full_output:
-        return iter_frames.numpy()
+        return iter_frames
     else:
-        return x_k.numpy()
+        return x_k
